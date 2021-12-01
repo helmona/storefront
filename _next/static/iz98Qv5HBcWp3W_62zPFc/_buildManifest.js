db4f494bf66ab3953eb2dad6edcbeb8a3742e41a
Inigo <garsanzi@gmail.com>
2021-12-01 09:50:24 +0100

    Updates

--- _next/static/iz98Qv5HBcWp3W_62zPFc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/61-dca02fca2b3349e5.js","static/chunks/pages/index-de23e1da42fbc010.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
