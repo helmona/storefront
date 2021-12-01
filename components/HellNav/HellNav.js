51ea1ad376aba6d158947d5420a29b1def6a8721
Inigo <garsanzi@gmail.com>
2021-12-01 23:09:44 +0100

    Fixing header padding and svg

--- components/HellNav/HellNav.js
+++ components/HellNav/HellNav.js
@@ -2,7 +2,7 @@ import Logo from '@components/Logo'
 
 const HellNav = () => {
   return (  
-    <nav className = "sticky flex flex-col top-0 z-50 bg-graywhite items-center md:items-start">
+    <nav className = "sticky flex flex-col top-0 z-50 bg-graywhite items-center md:items-start pt-2">
       <div className="flex flex-shrink-0 md:container md:mx-auto justify-center">
         <a href="#" title="Welcome to Helmona Storefront">
           <Logo />
